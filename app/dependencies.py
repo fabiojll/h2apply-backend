399ab6a99e71c981c6ba6d4e55c4a6b80087970e
fabiojll <147439571+fabiojll@users.noreply.github.com>
2025-11-01 16:09:17 -0300

    Add user authentication dependency with JWT

---
+++ app/dependencies.py
@@ -0,0 +1,32 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from app import models
+from app.database import get_db
+from app.config import settings
+from app.schemas import TokenData
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Não foi possível validar as credenciais",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(user_id=int(user_id))
+    except JWTError:
+        raise credentials_exception
+    user = db.query(models.User).filter(models.User.id == token_data.user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
